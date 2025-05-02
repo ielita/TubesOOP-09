9b048a39209639a7bcf797238969ff5004204d8f
Daniel <danielsipayungemail@gmail.com>
2025-05-02 23:20:18 +0700

    seperating player by creating entity

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -2,6 +2,8 @@ package entity;
 
 import main.GamePanel;
 import main.KeyHandler;
+import java.awt.Graphics2D;
+import java.awt.Color;
 
 public class Player extends Entity{
 
@@ -12,6 +14,8 @@ public class Player extends Entity{
 
         this.gp = gp;
         this.keyH = keyH;
+
+        setDefultValues();
     }
 
     public void setDefultValues(){
@@ -19,5 +23,28 @@ public class Player extends Entity{
         x = 200;
         y = 200;
         speed = 5;
+    }
+
+    public void update(){
+
+
+        if (keyH.upPressed == true){
+            y-=speed;
+        }
+        else if (keyH.downPressed == true){
+            y+=speed;
+        }
+        else if (keyH.leftPressed == true){
+            x-=speed;
+        }
+        else if (keyH.rightPressed == true){
+            x+=speed;
+        }
+    }
+
+    public void draw(Graphics2D g2){
+        g2.setColor(Color.white);
+
+        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
     }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -14,7 +14,7 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16; //16x16 tile
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale; //48x48 tile
+    public final int tileSize = originalTileSize * scale; //48x48 tile
     final int maxScreenCol = 16;    // horizontal 
     final int maxScreenRow = 12;    // vertikal 
     final int screenWidth = tileSize * maxScreenCol;    // 768 pixel
@@ -75,18 +75,8 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void update(){
-        if (keyH.upPressed == true){
-            playerY-=playerSpeed;
-        }
-        else if (keyH.downPressed == true){
-            playerY+=playerSpeed;
-        }
-        else if (keyH.leftPressed == true){
-            playerX-=playerSpeed;
-        }
-        else if (keyH.rightPressed == true){
-            playerX+=playerSpeed;
-        }
+        
+        player.update();
 
     }
 
@@ -95,9 +85,7 @@ public class GamePanel extends JPanel implements Runnable{
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D)g;
-        g2.setColor(Color.white);
-
-        g2.fillRect(playerX,playerY,tileSize,tileSize);
+        player.draw(g2);
         g2.dispose(); 
     }
 }
