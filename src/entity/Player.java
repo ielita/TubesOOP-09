10cc66da324387a2da3d8ab208b3e7002ef5ce91
Daniel <danielsipayungemail@gmail.com>
2025-05-03 02:41:35 +0700

    it really walk, not sliding again!

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -66,6 +66,17 @@ public void getPlayerImage() {
             x+=speed;
             direction = "right";
         }
+
+        spriteCounter++;
+        if (spriteCounter > 10){
+            if (spriteNum == 1){
+                spriteNum = 2;
+            }
+            else if(spriteNum == 2){
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
     }
 
     public void draw(Graphics2D g2){
@@ -77,16 +88,37 @@ public void getPlayerImage() {
 
         switch(direction){
             case "up":
-                image = up1;
+                if (spriteNum == 1){
+                    image = up1;
+                }
+                if (spriteNum == 2){
+                    image = up2;
+                }
+                
                 break;
             case "down":
-                image = down1;
+                if (spriteNum == 1){
+                    image = down1;
+                }
+                if (spriteNum == 2){
+                    image = down2;
+                }
                 break;
             case "left":
-                image = left1;
+                if (spriteNum == 1){
+                    image = left1;
+                }
+                if (spriteNum == 2){
+                    image = left2;
+                }
                 break;
             case "right":
-                image = right1;
+                if (spriteNum == 1){
+                    image = right1;
+                }
+                if (spriteNum == 2){
+                    image = right2;
+                }
                 break;
         }
 
