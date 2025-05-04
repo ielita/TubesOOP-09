3161fbf200a948b5aa5d6057e6f578536d71eaf7
Daniel <danielslearnstocode@gmail.com>
2025-05-04 18:17:58 +0700

    improve position

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -14,13 +14,15 @@ public class AssetSetter {
 
         gp.obj[0] =  new OBJ_Chest();
         gp.obj[0].worldX = 4 * gp.tileSize;
-        gp.obj[0].worldY = 45 * gp.tileSize;
-        // gp.obj[0].collisionOn = true;
+        gp.obj[0].worldY = 41 * gp.tileSize;
 
-        // gp.obj[1] =  new OBJ_Chest();
-        // gp.obj[1].worldX = 42 * gp.tileSize;
-        // gp.obj[1].worldY = 4 * gp.tileSize;
+        gp.obj[1] =  new OBJ_Chest();
+        gp.obj[1].worldX = 6 * gp.tileSize;
+        gp.obj[1].worldY = 41 * gp.tileSize;
         
+        gp.obj[2] =  new OBJ_Chest();
+        gp.obj[2].worldX = 8 * gp.tileSize;
+        gp.obj[2].worldY = 41 * gp.tileSize;
 
     }
 }
