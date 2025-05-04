575b9bf59ae611cfa80fc77dcb5b5a174c87e527
Daniel <danielslearnstocode@gmail.com>
2025-05-05 01:21:36 +0700

    change icon

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,31 @@
 package main;
 import javax.swing.JFrame;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.io.File;
+import javax.swing.ImageIcon;
+import java.io.FileNotFoundException;
 
 public class Main{
     
+
+
     public static void main(String[]args){
+
+        ImageIcon logo = null;
+        try {
+
+            logo = new ImageIcon(ImageIO.read(new File("res/icon/champion.png")));
+        } catch (IOException e) {
+            System.out.println("Error loading image: " + e.getMessage());
+            e.printStackTrace(); 
+        }
 
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("Spakbor si Petani");
+        window.setIconImage(logo.getImage());
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
